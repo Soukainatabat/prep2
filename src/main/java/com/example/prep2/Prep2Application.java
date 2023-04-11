4fd46bc4013c603c809cd3e92bc9df8b9fa6dcbb
Soukainatabat <stabat292001@gmail.com>
2023-04-11 16:00:00 +0200

    first commit

---
+++ src/main/java/com/example/prep2/Prep2Application.java
@@ -0,0 +1,13 @@
+package com.example.prep2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Prep2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Prep2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/prep2/Prep2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.prep2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Prep2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
